0bfba8d92a8324fd680064795c52ad00cc2fd167
Raffi Macaraig <raffi19.macaraig@gmail.com>
2024-05-24 15:58:42 -0700

    finished tic tac toe game with dom elements

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const gameboard = (function () {
     [".", ".", "."],
   ];
   let tilesRemaining = 9;
+  let playerOneTurn = true;
+  let gameOver = false;
 
   const updateBoard = (row, col, player) => {
     board[row][col] = player.getShape();
@@ -12,69 +14,132 @@ const gameboard = (function () {
   };
 
   const checkWin = (row, col, player) => {
-    console.log(board);
     for (let i = 0, j = col; i < 3; i++) {
-      if (board[i][j] !== player.getShape()) return false;
+      if (board[i][j] !== player.getShape()) break;
       else if (i == 2) return true;
     }
     for (let i = row, j = 0; j < 3; j++) {
-      if (board[i][j] !== player.getShape()) return false;
+      if (board[i][j] !== player.getShape()) break;
       else if (j == 2) return true;
     }
     for (let i = 0, j = 0; i < 3; i++, j++) {
-      if (board[i][j] !== player.getShape()) return false;
+      if (board[i][j] !== player.getShape()) break;
       else if (i == 2) return true;
     }
     for (let i = 2, j = 0; i >= 0; i--, j++) {
-      if (board[i][j] !== player.getShape()) return false;
+      if (board[i][j] !== player.getShape()) break;
       else if (i == 0) return true;
     }
+
+    return false;
+  };
+
+  const getTileContent = (row, col) => {
+    return board[row][col];
   };
 
   const getTilesRemaining = () => {
     return tilesRemaining;
   };
 
-  return { board, updateBoard, checkWin, getTilesRemaining };
+  const isPlayerOneTurn = () => {
+    return playerOneTurn;
+  };
+
+  const setPlayerOneTurn = (isTurn) => {
+    playerOneTurn = isTurn;
+  };
+
+  const setGameOver = (isGameOver) => {
+    gameOver = isGameOver;
+  };
+
+  const isGameOver = () => {
+    return gameOver;
+  };
+
+  const restartGame = () => {
+    for (let i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        board[i][j] = ".";
+      }
+    }
+
+    playerOneTurn = true;
+    gameOver = false;
+    tilesRemaining = 9;
+
+    const gameStatus = document.querySelector(".game-status");
+    gameStatus.innerHTML = "Player 1's Turn";
+  };
+
+  return {
+    updateBoard,
+    checkWin,
+    getTilesRemaining,
+    isPlayerOneTurn,
+    setPlayerOneTurn,
+    setGameOver,
+    isGameOver,
+    getTileContent,
+    restartGame,
+  };
 })();
 
-function createPlayer(playerNum, shape) {
-  this.playerNum = playerNum;
+function createPlayer(shape, playerNum) {
   this.shape = shape;
-
-  const getPlayerNum = () => {
-    return playerNum;
-  };
+  this.playerNum = playerNum;
 
   const getShape = () => {
     return shape;
   };
 
-  return { getPlayerNum, getShape };
+  const getPlayerNum = () => {
+    return playerNum;
+  };
+
+  return { getShape, getPlayerNum };
 }
 
-const playerOne = createPlayer(1, "X");
-const playerTwo = createPlayer(2, "O");
+const playerOne = createPlayer("X", 1);
+const playerTwo = createPlayer("O", 2);
 
-while (true) {
-  const row1 = prompt("Enter row");
-  const col1 = prompt("Enter col");
-  gameboard.updateBoard(row1, col1, playerOne);
+const tiles = document.querySelectorAll(".grid > div");
+const arr = [...tiles];
+tiles.forEach((tile) => {
+  tile.addEventListener("click", () => {
+    const idx = arr.indexOf(tile);
+    const row = parseInt(idx / 3);
+    const col = idx % 3;
+    if (gameboard.isGameOver() || gameboard.getTileContent(row, col) !== ".") {
+      return;
+    }
 
-  if (gameboard.checkWin(row1, col1, playerOne)) {
-    console.log("Player 1 Won!");
-    break;
-  } else if (gameboard.getTilesRemaining() == 0) {
-    console.log("It's a draw!");
-    break;
-  }
+    const player = gameboard.isPlayerOneTurn() ? playerOne : playerTwo;
+    const gameStatus = document.querySelector(".game-status");
+    tile.innerHTML = player.getShape();
+    gameboard.setPlayerOneTurn(!gameboard.isPlayerOneTurn());
+    gameStatus.innerHTML = `Player ${player.getPlayerNum()}'s Turn`;
+    gameboard.updateBoard(row, col, player);
 
-  //   const row2 = prompt("Enter row");
-  //   const col2 = prompt("Enter col");
-  //   gameboard.updateBoard(row2, col2, playerTwo);
+    if (gameboard.checkWin(row, col, player)) {
+      gameStatus.innerHTML =
+        "Game Over! Player " + player.getPlayerNum() + " wins!";
+      gameboard.setGameOver(true);
+      restartButton.style.visibility = "visible";
+    } else if (gameboard.getTilesRemaining() == 0) {
+      gameStatus.innerHTML = "Game Over! It's a tie!";
+      gameboard.setGameOver(true);
+      restartButton.style.visibility = "visible";
+    }
+  });
+});
 
-  //   if (gameboard.checkWin(row2, col2, playerTwo)) {
-  //     console.log("Player 2 Won!");
-  //     break;
-  //   }
-}
+const restartButton = document.querySelector("button");
+restartButton.addEventListener("click", () => {
+  gameboard.restartGame();
+  tiles.forEach((tile) => {
+    tile.innerHTML = "";
+  });
+  restartButton.style.visibility = "hidden";
+});
